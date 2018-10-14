f5555edb7d178280f1d2629047a44b86746fd565
Daniel Ehrenberg <littledan@chromium.org>
2018-10-14 19:24:49 +0200

    Set up continuous integration

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ev
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [[ "$TRAVIS_PULL_REQUEST" != "false" || "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]]; then
+  echo "Skipping deploy; just doing a build."
+  npm run build
+  exit 0
+fi
+
+# Enable SSH authentication
+
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+$(npm bin)/set-up-ssh --key "$ENCRYPTED_KEY" \
+                      --iv "$ENCRYPTED_IV" \
+                      --path-encrypted-key "$PRIVATE_KEY_FILE_NAME"
+
+# Update the content from the `gh-pages` branch
+
+$(npm bin)/update-branch --commands "npm run build" \
+                         --commit-message "Update gh-pages [skip ci]" \
+                         --directory "out" \
+                         --distribution-branch "gh-pages" \
+                         --source-branch "master"
+
